259d81dca4dca3be07d1b3a2394b4612033b46eb
Ethan Guest <eguest01@gmail.com>
2022-05-19 14:14:18 -0500

    add console app project for testing

---
+++ src/DNSBench-Console-App/Nameservers.cs
@@ -0,0 +1,60 @@
+﻿using System.Net.NetworkInformation;
+
+namespace DNSBench_Console_App;
+
+public class Nameservers
+{
+    public const string spaces = "                               ";
+
+    // Initialize list of DNS example servers
+    public Dictionary<(string providerAddress, string providerName), long> DnsExamples =
+        new()
+        {
+            {("156.154.70.22", "US - Comodo"), 0},
+            {("156.154.71.22", "US - Comodo"), 0},
+            {("8.8.4.4", "US - Google Public DNS"), 0},
+            {("8.8.8.8", "US - Google Public DNS"), 0},
+            {("204.74.101.1", "US - UltraDNS"), 0},
+            {("204.69.234.1", "US - UltraDNS"), 0},
+            {("208.67.222.222", "US - OpenDNS"), 0},
+            {("208.67.220.220", "US - OpenDNS"), 0}
+        };
+
+    public int TESTSTORUN = 10;
+
+
+    public void RunPingTest()
+    {
+        // list of strings to storing results
+        var results = new List<string>();
+
+
+        // Loop through 
+        foreach (var nameserver in DnsExamples)
+        {
+            // Create ping sender
+            var pingSender = new Ping();
+
+            // Ping the server
+            var reply = pingSender.Send(nameserver.Key.providerAddress);
+
+            // Add to the roundtrip time for the current nameserver
+            DnsExamples[nameserver.Key] += reply.RoundtripTime;
+
+
+            results.Add(
+                $"{nameserver.Key.providerName}{spaces.Remove(0, nameserver.Key.providerName.Length)}{nameserver.Key.providerAddress}{spaces.Remove(0, nameserver.Key.providerAddress.Length)}{reply.RoundtripTime}ms");
+        }
+
+        for (var i = 0; i < results.Count; i++) Console.WriteLine(results[i]);
+    }
+
+    // Display the results of the nameserver test
+    public void DisplayResults()
+    {
+        Console.WriteLine($"Host:{spaces.Remove(0, 6)} Address: {spaces.Remove(0, 9)}Ping:");
+        foreach (var dnsAddress in DnsExamples)
+            Console.WriteLine(
+                $"{dnsAddress.Key.providerName}{spaces.Remove(0, dnsAddress.Key.providerName.Length)}{dnsAddress.Key.providerAddress}{spaces.Remove(0, dnsAddress.Key.providerAddress.Length)}{dnsAddress.Value / TESTSTORUN}ms");
+    }
+}
---
+++ src/DNSBench-Console-App/Program.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DNSBench_Console_App;
+
+namespace MyApp;
+// Note: actual namespace depends on the project name.
+
+internal class Program
+{
+    // User
+    private static readonly User currentUser = new();
+
+
+    public static void MainLoop()
+    {
+        //currentUser.Initialize();
+        Console.WriteLine("Welcome to DNS Bench!");
+
+        Console.WriteLine("What operation would you like to perform?");
+
+        Console.WriteLine("1 = Check my DNS");
+
+        Console.WriteLine("2 = Perform DNS test");
+
+        Console.WriteLine("3 = Change DNS");
+
+        Console.WriteLine("4 = Exit");
+
+        var input = Console.ReadLine();
+
+        // Check user's current DNS
+        if (input == "1")
+        {
+            currentUser.Initialize();
+            Console.WriteLine("Your current primary DNS address: " + currentUser.primaryDNS);
+            Console.WriteLine("Your current secondary DNS address: " + currentUser.secondaryDNS);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+        // Run DNS benchmark
+        else if (input == "2")
+        {
+            // Instantiate the nameserver class
+            var nameservers = new Nameservers();
+
+            Console.Clear();
+
+            // Run tests
+            for (var i = 0; i < nameservers.TESTSTORUN; i++)
+            {
+                Console.WriteLine("[RUNNING TEST...]");
+                Console.WriteLine(
+                    $"Host:{Nameservers.spaces.Remove(0, 6)} Address: {Nameservers.spaces.Remove(0, 9)}Ping:");
+
+                nameservers.RunPingTest();
+                Thread.Sleep(100);
+                Console.Clear();
+            }
+
+            Console.WriteLine("[TEST COMPLETE]");
+
+            nameservers.DisplayResults();
+
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+        else if (input == "3")
+        {
+            Console.Write("Enter your new primaryDNS: ");
+            //userDns = Console.ReadLine();
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+        else if (input == "4")
+        {
+            return;
+        }
+
+        Console.Clear();
+    }
+
+
+    private static void Main(string[] args)
+    {
+        var exitConditon = false;
+        while (exitConditon != true) MainLoop();
+    }
+}
