8eee91cb56301003ff9bc655013b65b9d2c0287f
Ethan Guest <eguest01@gmail.com>
2022-05-22 17:28:46 -0500

    create viewmodels

---
+++ src/DNSUtility.Ui/ViewModels/NameserverListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using DNSUtility.Domain;
+
+namespace DNSUtility.Ui.ViewModels;
+
+public class NameserverListViewModel : ViewModelBase
+{
+    public NameserverListViewModel(IEnumerable<Nameserver> nameservers)
+    {
+        Nameservers = new ObservableCollection<Nameserver>(nameservers);
+    }
+
+    public ObservableCollection<Nameserver> Nameservers { get; set; }
+}
