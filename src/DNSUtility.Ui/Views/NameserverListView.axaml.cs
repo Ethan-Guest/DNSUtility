8a26de9adf3015f2991cd01d6120875ad9730f1a
Ethan Guest <eguest01@gmail.com>
2022-05-22 17:54:20 -0500

    add listview and populate with nameservers

---
+++ src/DNSUtility.Ui/Views/NameserverListView.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DNSUtility.Ui.Views;
+
+public partial class NameserverListView : UserControl
+{
+    public NameserverListView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
