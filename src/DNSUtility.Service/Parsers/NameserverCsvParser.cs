691e45d0fe8a72c6c61b7c7ac0f5d881e93625be
Ethan Guest <eguest01@gmail.com>
2022-05-22 16:36:50 -0500

    comment parse method, remove old Ui project

--- src/DNSUtility.Service/Parsers/NameserverCsvParser.cs
+++ src/DNSUtility.Service/Parsers/NameserverCsvParser.cs
@@ -8,16 +8,20 @@ namespace DNSUtility.Service.Parsers;
 public class NameserverCsvParser : IParser
 {
     /// <summary>
-    ///     Parse the CSV file and return a list of Nameserver
+    ///     Parse a CSV file.
     /// </summary>
     /// <param name="path"></param>
     /// <returns></returns>
     public IEnumerable<Nameserver> Parse(string path)
     {
+        // Create the webclient to read from URL
         var client = new WebClient();
         var stream = client.OpenRead(path);
+
+        // Read the file
         using var reader = new StreamReader(stream);
 
+        // Convert CSV to list of Nameserver using CsvHelper
         using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
         return csv.GetRecords<Nameserver>().ToList();
     }
