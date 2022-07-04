02a89de9c3cb94d0dec6f9559c31a6be62d06f35
Ethan <eguest01@gmail.com>
2022-07-03 20:02:12 -0500

    run the ping test multiple times on each nameserver

--- src/DNSUtility.Ui/ViewModels/NameserverListViewModel.cs
+++ src/DNSUtility.Ui/ViewModels/NameserverListViewModel.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Reactive;
+using System.Reactive.Linq;
 using System.Threading;
 using Avalonia.Collections;
 using Avalonia.Controls;
 using DNSUtility.Domain;
 using DNSUtility.Service.Benchmarks;
+using DynamicData.Binding;
 using ReactiveUI;
 
 namespace DNSUtility.Ui.ViewModels;
@@ -17,6 +20,7 @@ public class NameserverListViewModel : ViewModelBase
         Nameservers = new ObservableCollection<NameserverViewModel>();
 
         //int count = 0;
+
         
         foreach (var nameserver in nameservers)
         {
@@ -31,14 +35,17 @@ public class NameserverListViewModel : ViewModelBase
                     // Create a new thread for each test
                     new Thread(() =>
                     {
-                        // Run the test
-                        var ping = pingBenchmark.RunBenchmark(nameserver.IpAddress);
+                        // Run the test 5 times
+                        for (int i = 0; i < 5; i++)
+                        {
+                            ushort ping = pingBenchmark.RunBenchmark(nameserver.IpAddress);
                         
-                        // Set the reply as the latest ping
-                        nameserver.LatestPing = ping;
+                            // Set the reply as the latest ping
+                            nameserver.LatestPing = ping;
                         
-                        // Add the reply to the list of pings
-                        nameserver.Pings.Add(ping);
+                            // Add the reply to the list of pings
+                            nameserver.Pings.Add(ping);
+                        }
                     }).Start();
             });
     }
