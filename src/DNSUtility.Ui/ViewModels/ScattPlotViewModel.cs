52fa59afdc96051fbe0da8ddba977c83c70875d8
Ethan <eguest01@gmail.com>
2022-07-04 14:02:37 -0500

    add default scatter plot, adjust dataX

--- src/DNSUtility.Ui/ViewModels/ScattPlotViewModel.cs
+++ src/DNSUtility.Ui/ViewModels/ScattPlotViewModel.cs
@@ -11,19 +11,22 @@ public class ScattPlotViewModel : ViewModelBase
     public ScattPlotViewModel(NameserverViewModel nameserver)
     {
         ScatterPlot = new AvaPlot();
-        
-        double[] dataX = new double[nameserver.Pings.Count];
-        double[] dataY = new double[nameserver.Pings.Count];
-        
-        for (int i = 0; i < nameserver.Pings.Count; i++)
-        {
-            dataX[i] = i;
-            dataY[i] = nameserver.Pings[i];
-        }
 
-        //ScatterPlot = ScatterPlot.Find<AvaPlot>("ScattPlot");
-        ScatterPlot.Plot.AddScatter(dataX, dataY);
-        ScatterPlot.Refresh();
+        if (nameserver.Pings.Count != 0)
+        {
+            double[] dataX = new double[nameserver.Pings.Count];
+            double[] dataY = new double[nameserver.Pings.Count];
+        
+            for (int i = 0; i < nameserver.Pings.Count; i++)
+            {
+                dataX[i] = i + 1;
+                dataY[i] = nameserver.Pings[i];
+            }
+
+            //ScatterPlot = ScatterPlot.Find<AvaPlot>("ScattPlot");
+            ScatterPlot.Plot.AddScatter(dataX, dataY);
+            ScatterPlot.Refresh();
+        }
     }
     
     public AvaPlot ScatterPlot
