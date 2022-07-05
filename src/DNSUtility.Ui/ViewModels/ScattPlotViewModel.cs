3dc1e0b1da38132ec1924bd35c238538e4950ee6
Ethan <eguest01@gmail.com>
2022-07-05 10:57:07 -0500

    add more graph styles

--- src/DNSUtility.Ui/ViewModels/ScattPlotViewModel.cs
+++ src/DNSUtility.Ui/ViewModels/ScattPlotViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System.Drawing;
+using System.Drawing.Drawing2D;
 using ReactiveUI;
+using ScottPlot;
 using ScottPlot.Avalonia;
 
 namespace DNSUtility.Ui.ViewModels;
@@ -13,10 +15,27 @@ public class ScattPlotViewModel : ViewModelBase
         ScatterPlot = new AvaPlot();
 
         // Set the plot style
+        // Disable zooming
+        /*ScatterPlot.Configuration.Zoom = false;
+
+        // Disable panning
+        ScatterPlot.Configuration.Pan = false;*/
+
+        // Disable double click benchmark stats
+        ScatterPlot.Configuration.DoubleClickBenchmark = false;
+        
+        // Disable frame
+        ScatterPlot.Plot.Frameless();
+        
+        // Set axis limits
+        /*ScatterPlot.Plot.SetAxisLimits(yMin: 0);*/
+
+        
         ScatterPlot.Plot.Style(Color.Transparent,
             Color.FromArgb(1, 39, 39, 60),
             Color.FromArgb(53, 53, 83),
             Color.FromArgb(53, 53, 83));
+        
 
         if (nameserver.Pings.Count != 0)
         {
@@ -28,13 +47,17 @@ public class ScattPlotViewModel : ViewModelBase
                 dataX[i] = i + 1;
                 dataY[i] = nameserver.Pings[i];
             }
+            
+            var scatterPlotData = ScatterPlot.Plot.AddSignal(dataY,  1, Color.FromArgb(49, 255, 125));
+            
+            
+            scatterPlotData.FillBelow(Color.FromArgb(49, 255, 125), Color.FromArgb(125,49, 255, 125));
+            
+   
+            //scatterPlotData.Smooth = true;
 
-            //ScatterPlot = ScatterPlot.Find<AvaPlot>("ScattPlot");
-            ScatterPlot.Plot.AddScatter(dataX, dataY, Color.FromArgb(49, 255, 125), 3);
-
-
-            ScatterPlot.Plot.AddFill(dataX, dataY, color: Color.FromArgb(50, 49, 255, 125));
-            ScatterPlot.Plot.SetAxisLimits(yMin: 0);
+            /*ScatterPlot.Plot.AddFill(dataX, dataY, color: Color.FromArgb(50, 49, 255, 125));
+*/
             ScatterPlot.Refresh();
         }
     }
