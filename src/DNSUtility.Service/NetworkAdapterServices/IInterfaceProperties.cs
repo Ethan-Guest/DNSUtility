ed36458ab310cd5f55472c6aa4a7eb5dc52b27b1
Ethan <eguest01@gmail.com>
2022-07-05 12:22:27 -0500

    add service for loading interface properties

---
+++ src/DNSUtility.Service/NetworkAdapterServices/IInterfaceProperties.cs
@@ -0,0 +1,8 @@
+﻿using System.Net.NetworkInformation;
+
+namespace DNSUtility.Service.NetworkAdapterServices;
+
+public interface IInterfaceProperties
+{
+    IPInterfaceProperties GetInterfaceProperties(NetworkInterface adapter);
+}
---
+++ src/DNSUtility.Service/NetworkAdapterServices/LoadInterfaceProperties.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.NetworkInformation;
+
+namespace DNSUtility.Service.NetworkAdapterServices;
+
+public class LoadInterfaceProperties : IInterfaceProperties
+{
+    public IPInterfaceProperties GetInterfaceProperties(NetworkInterface adapter)
+    {
+        return adapter.GetIPProperties();
+    }
+}
