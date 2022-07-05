c2a8a74689f62cc69d32affe96650d4920efff02
Ethan <eguest01@gmail.com>
2022-07-04 20:56:53 -0500

    update appearance of scatter plot

--- src/DNSUtility.Ui/ViewModels/ScattPlotViewModel.cs
+++ src/DNSUtility.Ui/ViewModels/ScattPlotViewModel.cs
@@ -1,4 +1,4 @@
-﻿using Avalonia.Controls;
+﻿using System.Drawing;
 using ReactiveUI;
 using ScottPlot.Avalonia;
 
@@ -7,33 +7,41 @@ namespace DNSUtility.Ui.ViewModels;
 public class ScattPlotViewModel : ViewModelBase
 {
     private AvaPlot _scatterPlot;
-    
+
     public ScattPlotViewModel(NameserverViewModel nameserver)
     {
         ScatterPlot = new AvaPlot();
 
+        // Set the plot style
+        ScatterPlot.Plot.Style(Color.Transparent,
+            Color.FromArgb(1, 39, 39, 60),
+            Color.FromArgb(53, 53, 83),
+            Color.FromArgb(53, 53, 83));
+
         if (nameserver.Pings.Count != 0)
         {
-            double[] dataX = new double[nameserver.Pings.Count];
-            double[] dataY = new double[nameserver.Pings.Count];
-        
-            for (int i = 0; i < nameserver.Pings.Count; i++)
+            var dataX = new double[nameserver.Pings.Count];
+            var dataY = new double[nameserver.Pings.Count];
+
+            for (var i = 0; i < nameserver.Pings.Count; i++)
             {
                 dataX[i] = i + 1;
                 dataY[i] = nameserver.Pings[i];
             }
 
             //ScatterPlot = ScatterPlot.Find<AvaPlot>("ScattPlot");
-            ScatterPlot.Plot.AddScatter(dataX, dataY);
+            ScatterPlot.Plot.AddScatter(dataX, dataY, Color.FromArgb(49, 255, 125), 3);
+
+
+            ScatterPlot.Plot.AddFill(dataX, dataY, color: Color.FromArgb(50, 49, 255, 125));
+            ScatterPlot.Plot.SetAxisLimits(yMin: 0);
             ScatterPlot.Refresh();
         }
     }
-    
+
     public AvaPlot ScatterPlot
     {
         get => _scatterPlot;
-        set => this.RaiseAndSetIfChanged(ref _scatterPlot, value);    
+        set => this.RaiseAndSetIfChanged(ref _scatterPlot, value);
     }
-
-
 }
