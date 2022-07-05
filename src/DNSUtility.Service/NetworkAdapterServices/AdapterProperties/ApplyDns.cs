dfe5d60a5e22d725f38faafdca0bdb77100b5a0a
Ethan <eguest01@gmail.com>
2022-07-05 13:20:04 -0500

    add service for applying dns

---
+++ src/DNSUtility.Service/NetworkAdapterServices/AdapterProperties/ApplyDns.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.NetworkInformation;
+using DNSUtility.Domain;
+
+namespace DNSUtility.Service.NetworkAdapterServices;
+
+public class ApplyDns : IApplyDns
+{
+    public void ApplyPreferredDns(Nameserver nameserver, NetworkInterface adapter)
+    {
+        // Set the dns server
+    }
+
+    public void ApplyAlternateDns(Nameserver nameserver, NetworkInterface adapter)
+    { 
+        // Set the dns server
+    }
+}
---
+++ src/DNSUtility.Service/NetworkAdapterServices/AdapterProperties/IApplyDns.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.NetworkInformation;
+using DNSUtility.Domain;
+
+namespace DNSUtility.Service.NetworkAdapterServices;
+
+public interface IApplyDns
+{
+    void ApplyPreferredDns(Nameserver nameserver, NetworkInterface adapter);
+    
+    void ApplyAlternateDns(Nameserver nameserver, NetworkInterface adapter);
+}
