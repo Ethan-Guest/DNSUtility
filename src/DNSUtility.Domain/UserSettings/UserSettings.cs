82aca38a4127452e04b8a1085c2ed711f1d55531
Ethan <eguest01@gmail.com>
2022-07-07 10:52:50 -0500

    create user settings domain class

---
+++ src/DNSUtility.Domain/UserSettings/UserSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace DNSUtility.Domain.UserSettings;
+
+public class UserSettings
+{
+    public UserSettings(string country, string language)
+    {
+        Country = country;
+        Language = language;
+    }
+
+    public string Country { get; set; }
+
+    public string Language { get; set; }
+}
