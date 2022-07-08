6c559d3a019831bf548ab53f353f93b626d3222c
Ethan <eguest01@gmail.com>
2022-07-08 14:19:20 -0500

    add AutoMapper and create configuration

---
+++ src/DNSUtility.Ui/AutoMapperConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace DNSUtility.Ui;
+
+public class AutoMapperConfig
+{
+    public void Configure()
+    {
+        /*var config = new MapperConfiguration(cfg => cfg.CreateMap<, Nameserver>());
+        config.CreateMapper();*/
+    }
+}
