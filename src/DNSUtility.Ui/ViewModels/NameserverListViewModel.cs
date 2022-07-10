db65077f51e163ace464da7a0add86012fde5d73
Ethan <eguest01@gmail.com>
2022-07-10 15:07:53 -0500

    refactor dns benchmark, closes #4

--- src/DNSUtility.Ui/ViewModels/NameserverListViewModel.cs
+++ src/DNSUtility.Ui/ViewModels/NameserverListViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Reactive;
 using System.Reactive.Linq;
-using System.Threading;
+using System.Threading.Tasks;
 using DNSUtility.Domain.AppModels;
 using DNSUtility.Service.Benchmarks;
 using DNSUtility.Service.NetworkAdapterServices.AdapterProperties;
@@ -31,36 +31,19 @@ public class NameserverListViewModel : ViewModelBase
             Nameservers.Add(nameserverViewModel);
         }
 
-        RunDnsTest = ReactiveCommand.Create(
-            () =>
-            {
-                foreach (var nameserver in Nameservers)
-                    // Create a new thread for each test
-                    new Thread(() =>
-                    {
-                        // Run the test 5 times
-                        for (int i = 0; i < 5; i++)
-                        {
-                            var ping = pingBenchmark.Run(nameserver.IpAddress);
+        RunDnsTest = ReactiveCommand.CreateFromTask(() =>
+        {
+            // Create a list of tasks
+            var benchmarkTasks = new List<Task>();
 
-                            // Set the reply as the latest ping
-                            nameserver.LatestPing = ping;
+            // Create a benchmark task for each nameserver
+            foreach (var nameserver in Nameservers) benchmarkTasks.Add(BenchmarkNameserver(nameserver, pingBenchmark));
 
-                            // Add the reply to the list of pings
-                            nameserver.Pings.Add(ping);
+            RemovePoorQualityNameservers();
 
-                            // If the ping replies with 0, remove it from the list
-                            /*if (ping == 0)
-                            {
-                                Nameservers.Remove(nameserver);
-                                break;
-                            }*/
-                        }
-
-                        // Calculate the average ping so it can be displayed in the view
-                        nameserver.CalculateAveragePing();
-                    }).Start();
-            });
+            // Wait for all benchmarks to be completed
+            return Task.WhenAll(benchmarkTasks.ToArray());
+        });
 
         // TODO: Merge the following TWO commands into ONE function with different parameters 
         // Command for applying primary dns server
@@ -98,6 +81,12 @@ public class NameserverListViewModel : ViewModelBase
     public ReactiveCommand<Unit, Unit> ApplySecondary { get; set; }
 
 
+    // The minimum average ping found in the nameserver list. Used to determine the status of the nameserver
+    public int MinPingAverage { get; set; }
+
+    // The maximum average ping found in the nameserver list. Used to determine the status of the nameserver
+    public int MaxPingAverage { get; set; }
+
     public NameserverViewModel? SelectedNameserver
     {
         get => _selectedNameserver;
@@ -105,6 +94,44 @@ public class NameserverListViewModel : ViewModelBase
     }
 
     public MainWindowViewModel MainViewModel { get; set; }
+
+    private Task BenchmarkNameserver(NameserverViewModel nameserver, IBenchmark pingBenchmark)
+    {
+        return Task.Run(() =>
+        {
+            for (var j = 0; j < 5; j++)
+            {
+                var ping = pingBenchmark.Run(nameserver.IpAddress);
+
+                // Set the reply as the latest ping
+                nameserver.LatestPing = ping;
+
+                // Add the reply to the list of pings
+                nameserver.Pings.Add(ping);
+
+                // Calculate the average ping so it can be displayed in the view
+                nameserver.CalculateAveragePing();
+
+                if (nameserver.AveragePing == 0) return Task.CompletedTask;
+            }
+
+            return Task.CompletedTask;
+        });
+    }
+
+    /// <summary>
+    ///     A method to remove the poor quality nameservers.
+    /// </summary>
+    private void RemovePoorQualityNameservers()
+    {
+        for (var i = 0; i < Nameservers.Count; i++)
+            if (Nameservers[i].LatestPing == 0)
+                Nameservers.RemoveAt(i);
+    }
+
+    private void RunTests(IBenchmark pingBenchmark, NameserverViewModel nameserver)
+    {
+    }
 
     private async void CreatePlot(NameserverViewModel? nameserver)
     {
