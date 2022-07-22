c89b1a71c24cb3b262cf1c9ee0565d7741276457
Ethan <eguest01@gmail.com>
2022-07-22 06:07:47 -0500

    add constructor in livechart viewmodel

--- src/DNSUtility.Ui/ViewModels/LiveChartViewModel.cs
+++ src/DNSUtility.Ui/ViewModels/LiveChartViewModel.cs
@@ -1,9 +1,23 @@
 ﻿using System.Collections.ObjectModel;
+using LiveChartsCore;
 using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
 
 namespace DNSUtility.Ui.ViewModels;
 
 public class LiveChartViewModel : ViewModelBase
 {
-    private readonly ObservableCollection<ObservableValue> _observable;
+    private readonly ObservableCollection<ObservableValue> _observableValues;
+
+    public LiveChartViewModel()
+    {
+        _observableValues = new ObservableCollection<ObservableValue>();
+
+        Series = new ObservableCollection<ISeries>
+        {
+            new LineSeries<ObservableValue>()
+        };
+    }
+
+    public ObservableCollection<ISeries> Series { get; set; }
 }
