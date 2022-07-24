c43b02975a6f03ff944aa65850ca4140b2acd0b8
Ethan <eguest01@gmail.com>
2022-07-24 05:41:03 -0500

    create models directory with sub-folders

---
+++ src/DNSUtility.Domain/Models/App/Nameserver.cs
@@ -0,0 +1,18 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace DNSUtility.Domain.AppModels;
+
+public class Nameserver
+{
+    // The nameserver class is a standard model class with CsvHelper name attributes
+    // that map the headers of a csv file into their corresponding data types
+    [Name("ip_address")] public string IpAddress { get; set; }
+
+    [Name("as_org")] public string Name { get; set; }
+
+    [Name("country_code")] public string Country { get; set; }
+
+    [Name("dnssec")] public bool Dnssec { get; set; }
+
+    [Name("reliability")] public decimal Reliability { get; set; }
+}
